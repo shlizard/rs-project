201234810000fc15146a692d68ef20e888566bc0
shayan <shayaneshraqi@gmail.com>
2025-01-26 17:20:17 +0330

    build commands fixed

--- build.sh
+++ build.sh
@@ -4,5 +4,8 @@ git checkout master
 echo "Building app..."
 npm run build
 
+echo "copying the build folder to public folder"
+cp -r build/* ./main
+
 echo "running the server"
 python manage.py runserver
