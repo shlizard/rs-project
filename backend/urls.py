c9ca590dc3659fca8f77f1c178c09b1e15879ff0
shayan <shayaneshraqi@gmail.com>
2025-07-12 16:06:48 +0330

    .gitignore update

--- backend/urls.py
+++ backend/urls.py
@@ -18,10 +18,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path("api/",include("main.urls")),
+    path("api/", include("main.urls")),
     path("api/user", include("user.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("admin/", admin.site.urls),
     re_path("", TemplateView.as_view(template_name="index.html")),
 ]
